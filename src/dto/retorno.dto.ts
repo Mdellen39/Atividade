4d076cae3c6052b629c060e674456ebf2c8a314b
Mdellen39 <matheus.mrodrigues6@gmail.com>
2024-10-23 21:09:27 -0300

    coomit
    coomit

---
+++ src/dto/retorno.dto.ts
@@ -0,0 +1,13 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+
+
+export class RetornoCadastroDTO{
+    id: string;
+    message: string;
+}
+
+export class RetornoObjDTO{
+    return: any;
+    message: string;
+}
+
